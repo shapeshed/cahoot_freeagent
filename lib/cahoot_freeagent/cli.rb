a4900dac228d07441e9c18321814584a519e16be
George Ornbo <george@shapeshed.com>
2010-12-06 21:38:15 +0000

    0.0.1 release

--- lib/cahoot_freeagent/cli.rb
+++ lib/cahoot_freeagent/cli.rb
@@ -1,9 +1,34 @@
 require 'thor'
+require 'cahoot_freeagent/csv'
+
 module CahootFreeagent
   class CLI < Thor
-    desc "portray ITEM", "Determines if a piece of food is gross or delicious"
-    def portray
-        puts 'portray method'
+
+    default_task :parse_html
+
+    desc "Help", "Help text for the command line tool"
+    def help
+        puts 'This tool parses Cahoot HTML statements to a FreeAgentCentral compatible CSV file'
+        puts 'Download statements from Cahoot and save as HTML files'
+        puts 'Convert the files using the following syntax'
+        puts 'cahoot_freeagent --files statement1.html statement2.html'
+    end
+
+    desc "Parse HTML", "Parses a Cahoot HTML statement and outputs an FreeAgent compatible CSV"
+    method_options :files => :array
+    def parse_html
+      if options.files?
+        options.files.each do |file|
+          if FileTest.exists?(file)
+            CahootFreeagent::CSV.new(file)
+          else
+            puts "The file #{file} was not found so was not converted"
+          end
+        end
+      else
+        puts 'Please specify some files to convert'
+        puts 'USAGE: cahoot_freeagent --files statement.html'
+      end
     end
   end
 end
